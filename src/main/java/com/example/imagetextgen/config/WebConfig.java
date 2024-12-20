1d76e450371d3797237640602cca71061fde2aaa
parkmaeil <bitcocom@empas.com>
2024-12-20 17:38:55 +0900

    imagetextgen

---
+++ src/main/java/com/example/imagetextgen/config/WebConfig.java
@@ -0,0 +1,20 @@
+package com.example.imagetextgen.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Value("${upload.path}")
+    private String uploadPath;
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/uploads/**")  // URL 패턴
+                .addResourceLocations("file:" + uploadPath);  // 파일 시스템의 실제 경로
+    }
+}
+
